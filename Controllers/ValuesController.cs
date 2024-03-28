8d4f0d1e4f5034a75624f06ce2c9e4d2a1d2007b
Mateo-RENAC <mateo.renac@gmail.com>
2024-03-28 14:16:29 +0100

    SuperHeroService (normalement terminé)

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Super_Hero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
